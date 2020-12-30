576662c09b1fc5937cc8d18623da1a81abe302ae
Daniel <daniel@kali.Dirtymonk>
2020-12-30 20:57:32 +0700

    first commit

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+
+# from .views import
+
+# urlpatterns = [
+#     path('all/', GetAllUsers.as_view(), name='get_all_users'),
+#     path('one_product/', GetUserForCart.as_view(), name='get_one_users'),
+# ]
